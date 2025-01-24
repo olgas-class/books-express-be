f115aeb45ade8191a123d5163f2e7cfb21ce093d
olga_demina <olga.demina1410@gmail.com>
2025-01-24 12:00:44 +0100

    feat: cors middleware added

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require("express");
 const booksRouter = require("./routers/books");
 const errorsHandler = require("./middleware/errorsHandler");
 const notFound = require("./middleware/notFound");
+const cors = require("cors");
 
 // creazione dell'app express
 const app = express();
 const port = process.env.SERVER_PORT;
+
+// Middleware delle CORS
+app.use(cors({
+  origin: process.env.FRONTENT_URL
+}))
 
 // Middleware per rendere la cartella pubblica accessibile da fuori
 app.use(express.static('public'));
